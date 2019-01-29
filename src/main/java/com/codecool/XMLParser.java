5ac3e0956b1a6f0bb12ab6667509bafed96b2b5b
Pál Monoczki <pal.monoczki@codecool.com>
2019-01-29 19:24:21 +0100

    XMLParser has been added.

---
+++ src/main/java/com/codecool/XMLParser.java
@@ -0,0 +1,12 @@
+package com.codecool;
+
+import org.w3c.dom.Document;
+
+public abstract class XMLParser {
+
+    public Document loadXMLDocument(String path) {
+
+        return null;
+        Ż
+    }
+}
