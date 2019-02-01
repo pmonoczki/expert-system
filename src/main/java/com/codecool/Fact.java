aac009eb5d4b6f364bbbcc312a122c252a57798b
Pál Monoczki <pal.monoczki@codecool.com>
2019-02-01 05:57:43 +0100

    FactRepository is implemented.

--- src/main/java/com/codecool/Fact.java
+++ src/main/java/com/codecool/Fact.java
@@ -1,4 +1,37 @@
 package com.codecool;
 
+import java.util.*;
+
 public class Fact {
+
+
+    private String description;
+
+    private Set<String> evals;
+
+    private Map<String, Boolean> fa;
+
+    public Fact(String sescription) {
+        this.description = description;
+        this.evals = new HashSet<String>();
+        this.fa = new HashMap<String, Boolean>();
+    }
+
+    public Set<String> getIdSet() {
+        return this.evals;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public void setFactValueById(String id, boolean value) {
+        fa.put(id, Boolean.valueOf(value));
+    }
+
+    public boolean getValueById(String id) {
+        return this.fa.get(id).booleanValue();
+    }
+
+
 }
--- src/main/java/com/codecool/FactRepository.java
+++ src/main/java/com/codecool/FactRepository.java
@@ -1,4 +1,18 @@
 package com.codecool;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 public class FactRepository {
+
+    private List<Fact> facts;
+
+    public FactRepository() {
+        facts = new ArrayList<Fact>();
+    }
+
+    public Iterator getIterator() {
+        return this.facts.iterator();
+    }
 }
